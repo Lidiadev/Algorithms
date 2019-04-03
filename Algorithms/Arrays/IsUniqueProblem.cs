7ae9b66e287bc411090b674b6136095f5eea39db
Lidia Necula <LNecula@binck.nl>
2019-04-03 12:17:06 +0200

    Add IsUniqueProblem

---
+++ Algorithms/Arrays/IsUniqueProblem.cs
@@ -0,0 +1,30 @@
+﻿namespace Algorithms.Arrays
+{
+    /// <summary>
+    /// Is Unique: Implement an algorithm to determine if a string has all unique characters.
+    /// What if you cannot use additional data structures?
+    /// </summary>
+    public static class IsUniqueProblem
+    {
+        /// <summary>
+        /// Time O(n) Space O(1)
+        /// </summary>
+        public static bool HasUniqueCharacters(string text)
+        {
+            if(string.IsNullOrEmpty(text) || text.Length > 256)
+                return false;
+
+            bool[] chars = new bool[256];
+
+            foreach (var c in text)
+            {
+                if (chars[c])
+                    return false;
+
+                chars[c] = true;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ AlgorithmsUnitTests/Arrays/IsUniqueProblemTests.cs
@@ -0,0 +1,25 @@
+﻿namespace AlgorithmsUnitTests.Arrays
+{
+    using Algorithms.Arrays;
+    using NUnit.Framework;
+
+    public class IsUniqueProblemTests
+    {
+        [Test]
+        [TestCase("abcdef0*-h")]
+        [TestCase("ab")]
+        [TestCase("a")]
+        public void HasUniqueCharacters_WhenStringHasUniqueChars_ShouldReturnTrue(string text)
+            => Assert.True(IsUniqueProblem.HasUniqueCharacters(text));
+
+        [Test]
+        [TestCase("abcdeaf")]
+        [TestCase("abba")]
+        [TestCase("aa")]
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase("2yNbC9V34QAcHdPhYsh2MmGTdKrKVsZfHYpsrL2bY7ohxof0pHzKXHWlshvcGrgtFjqSa9yTS84BBw8B5vm7tWZXj73FfiUOmxddvIW7UXwHbyx7PQJMAUnsZWNXAxuSx2EFSOmP7bZ6vTWvGoCvqcsFtvQpTeWqPnJtwEeq3oA9Md6UszuQalRD2yRgN5uhzITAOthWSNE1MDsyi2heJWvBtpe0nt7tNRnQbIRzvEGhRKlYhV3fZI1BO6oECdzZt")]
+        public void HasUniqueCharacters_WhenStringHasDuplicatesOrStringIsNullOrEmpty_ShouldReturnFalse(string text)
+            => Assert.False(IsUniqueProblem.HasUniqueCharacters(text));
+    }
+}
