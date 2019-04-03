217609c2616c2bc5788c25cb8e615584e98a0df6
Lidia Necula <LNecula@binck.nl>
2019-04-03 22:10:48 +0200

    Add PermutationProblem
    Signed-off-by: Lidia Necula <LNecula@binck.nl>

---
+++ Algorithms/Arrays/PermutationProblem.cs
@@ -0,0 +1,34 @@
+﻿namespace Algorithms.Arrays
+{
+    public static class PermutationProblem
+    {
+        /// <summary>
+        /// Check Permutation: Given two strings, write a method to decide
+        /// if one is a permutation of the other
+        /// </summary>
+        public static bool IsPermutation(string a, string b)
+        {
+            if (a == null || b == null)
+                return false;
+
+            if (a.Length != b.Length)
+                return false;
+
+            int[] characters = new int[256];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                characters[a[i]]++;
+                characters[b[i]]--;
+            }
+
+            for (int i = 0; i < 256; i++)
+            {
+                if (characters[i] != 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ AlgorithmsUnitTests/Arrays/PermutationProblemTests.cs
@@ -0,0 +1,30 @@
+﻿namespace AlgorithmsUnitTests.Arrays
+{
+    using Algorithms.Arrays;
+    using NUnit.Framework;
+
+    public class PermutationProblemTests
+    {
+        [Test]
+        [TestCase("elvis", "lives")]
+        [TestCase("abababab", "babababa")]
+        [TestCase("aaaaa", "aaaaa")]
+        [TestCase("a", "a")]
+        [TestCase("rail safety", "fairy tales")]
+        [TestCase("", "")]
+        public void IsPermutation_WhenStringsHaveIdenticalChars_ShouldReturnTrue(string text1, string text2)
+            => Assert.True(PermutationProblem.IsPermutation(text1, text2));
+
+        [Test]
+        [TestCase("aaaa", "aaaaa")]
+        [TestCase("aab", "aaab")]
+        [TestCase("a*", "a")]
+        [TestCase("a", "b")]
+        [TestCase("railsafety", "fairytales ")]
+        [TestCase("railsafety", " fairytales")]
+        [TestCase("railsafety", " fairy tales")]
+        [TestCase(null, "")]
+        public void IsPermutation_WhenStringsDontHaveIdenticalChars_ShouldReturnFalse(string text1, string text2)
+            => Assert.False(PermutationProblem.IsPermutation(text1, text2));
+    }
+}
